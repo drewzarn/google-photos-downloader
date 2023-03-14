085db611baeb8f0a0b113abcbbee9edddade0822
Drew Zarn <drew@abmp.com>
2023-03-14 15:23:39 -0600

    Add action arg

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { OAuth2Client } = require("google-auth-library");
 const { authenticate } = require("./lib/authenticate");
 const { downloadMedia } = require("./lib/download-media");
 
+const action = process.argv[2] ?? "download";
+
 authenticate().then(oAuth2Client => {
     console.log("OAuth2Client:", oAuth2Client);
-    downloadMedia(oAuth2Client);
+    downloadMedia(oAuth2Client, action);
 });
