b738f81637dbd8cece2f31a46a5e401cee6a56de
Drew Zarn <drew@abmp.com>
2023-03-17 09:15:22 -0600

    Save last page token Don't re-add photos to DB

--- index.js
+++ index.js
@@ -3,25 +3,30 @@ const { authenticate } = require("./lib/authenticate");
 const photos = require("./lib/photos");
 const db = require("./lib/db");
 
-const action = process.argv[2]?.toLowerCase() ?? "download";
+const action = process.argv[2]?.toLowerCase() ?? "synclibrary";
 
 switch (action) {
     case "synclibrary":
         db.getCredentials().then(credentials => {
             console.log(credentials);
-            authenticate(credentials).then(oAuth2Client => {
+            authenticate(credentials).then(async oAuth2Client => {
                 db.addCredentials(oAuth2Client.credentials);
-                photos.getMediaItemsPage(oAuth2Client);
+                let lastPageToken = await db.getLastPageToken();
+                console.log(lastPageToken);
+                photos.getMediaItemsPage(oAuth2Client, lastPageToken);
             });
         });
         break;
     case "download":
         break;
+    case "resetsyncpage":
+        db.resetPageToken();
+        break;
     case "cleanup":
         db.cleanup();
         break;
     case "resetauth":
-        db.resetTokens();
+        db.resetAuth();
         break;
     default:
         console.error("Unknown action", action);
